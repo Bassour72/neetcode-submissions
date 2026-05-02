1017a978dd382b9159e2a06dd13d75375597c8ce
Younes Bassour <114539362+Bassour72@users.noreply.github.com>
2026-05-02 17:03:04 +0100

    Bulk sync: 5 submissions

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) 
+    {
+        string str;
+        string temp;
+        for (size_t i = 0; i < strs.size(); i++)
+        
+            str += strs[i] + "##" ;
+        return(str);
+    }
+    vector<string> decode(string s) 
+    {
+        vector<string> strs;
+        size_t pos = - 1;
+        while ( (pos = s.find('#')) != string::npos)
+        {
+            // if (s[pos - 1] != '2')
+            //     continue;
+            if (s != "")
+                strs.push_back(s.substr(0, pos - 1));
+            s.erase(0, pos + 1);
+        }
+        if (!s.empty())
+            strs.push_back(s);
+        return(strs);
+    }
+};
